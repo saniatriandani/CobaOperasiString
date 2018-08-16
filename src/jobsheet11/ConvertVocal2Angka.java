213a635081d05080a1798918b5c24680b5edaffc
LENOVO <LENOVO@LENOVO-PC>
2018-08-16 19:23:02 +0700

    Tambah Konversi

--- src/jobsheet11/ConvertVocal2Angka.java
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -17,7 +17,7 @@ public class ConvertVocal2Angka {
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
-private static String tampilInput() {
+    private static String tampilInput() {
         Scanner scanner = new Scanner(System.in);
        
             System.out.println("Masukkan Kalimat: ");
@@ -26,11 +26,25 @@ private static String tampilInput() {
             
             return kalimat;       
     }
+    private static String vocal2Angka(String kalimat){
+        char [][] arConvert =
+        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+                
+            kalimat = kalimat.toLowerCase();
+            for(int i=0; i < arConvert.length; i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+                   
+            return kalimat;
+    }
+
     public static void main(String[] args){
     String identitas = "sania tri andani / x rpl 3 / 35";
     
     tampilJudul(identitas);
-    String kalimat = tampilInput();
+   
+        String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
+          
     }
 }
 
